6a2e48d0db61dc3910d426ad9a59af881d703a02
Code-Cruncher-Omega <earella6@gmail.com>
2025-12-08 19:43:16 -0500

    Implemented "mkdir" and "rmdir," updated "help" command, and replaced some try-except blocks of code with if-else blocks
    Removed try-except in "cd," "mv," "cat," and "cp"
    
    Also updated the indentation of "cat"

--- python_shell.py
+++ python_shell.py
@@ -9,6 +9,8 @@ def help_menu():
     print("cd <directory>\t-\t Changes the current working directory to <directory>")
     print("ls\t-\t Prints the current working directory contents")
     print("pwd\t-\t Prints the current working directory")
+    print("mkdir\t-\t Creates the directory <directory>")
+    print("rmdir\t-\t Removes the directory <directory> only if it is empty")
     print("mv <src> <dst>\t-\t Moves <src> to <dst>")
     print("cat <file>\t-\t Prints the contents of text file <file>")
     print("cp <src> <dst>\t-\t Copies <src> to <dst>")
@@ -46,51 +48,96 @@ def trace_command(cmd_line: str):
     #Try check for specific builtins (cd, pwd). //ADD ANY ADDITIONAL COMMANDS HERE// 
     try:
         if args[0].lower() == "cd":
-            try:
-                os.chdir(args[1])
-                print(f"Changed directory to: {args[1]}")
-            except IndexError:
+            if len(args) < 2:
                 print("Usage: cd <directory>")
+                return
 
+            if not os.path.exists(args[1]):
+                print(f"No such directory: {args[1]}")
+                return
+
+            os.chdir(args[1])
+            print(f"Changed directory to: {args[1]}")
             return
         if args[0].lower() == "ls":
             print("Current directory contents:")
             print(os.listdir(os.getcwd()))
+            return
         if args[0].lower() == "pwd":
             print("Current working directory: ", os.getcwd())
+            return
+        if args[0].lower() == "mkdir":
+            if len(args) < 2:
+                print("Usage: mkdir <directory>")
+                return
+
+            if os.path.exists(args[1]):
+                print(f"Directory already exists: {args[1]}")
+            else:
+                os.mkdir(args[1])
+                print(f"Created directory: {args[1]}")
+
+            return
+        if args[0].lower() == "rmdir":
+            if len(args) < 2:
+                print("Usage: rmdir <directory>")
+                return
+
+            if not os.path.exists(args[1]):
+                print(f"Directory does not exist: {args[1]}")
+                return
+
+            if len(os.listdir(args[1])) == 0:
+                os.rmdir(args[1])
+                print(f"Directory removed: {args[1]}")
+            else:
+                print(f"Directory not empty: {args[1]}")
+
             return
         if args[0].lower() == "mv":
             if len(args) < 3:
                 print("Usage: mv <src> <dst>")
                 return
-            try:
-                if os.path.exists(args[2]):
-                    shutil.move(args[1], args[2])
-                    print(f"Moved {args[1]} -> {args[2]}")
-                else:
-                    print(f"No such directory: {args[2]}")
-            except FileNotFoundError:
+
+            if not os.path.exists(args[1]):
                 print(f"File not found: {args[1]}")
+                return
+
+            if not os.path.exists(args[2]):
+                print(f"No such directory: {args[2]}")
+                return
+
+            shutil.move(args[1], args[2])
+            print(f"Moved {args[1]} -> {args[2]}")
             return
         if args[0].lower() == "cat":
-                if len(args) < 2:
-                    print("Usage: cat <file>")
-                    return
-                try:
-                    with open(args[1], "r") as f:
-                            print(f.read())
-                except FileNotFoundError:
-                    print(f"File not found: {args[1]}")
+            if len(args) < 2:
+                print("Usage: cat <file>")
                 return
+
+            try:
+                with open(args[1], "r") as file:
+                    print(file.read())
+                    file.close()
+            except FileNotFoundError:
+                print(f"File not found: {args[1]}")
+
+            return
         if args[0].lower() == "cp":
             if len(args) < 3:
                 print("Usage: cp <src> <dst>")
                 return
-            try:
-                shutil.copy(args[1], args[2])
-                print(f"Copied {args[1]} -> {args[2]}")
-            except FileNotFoundError:
+
+            if not os.path.exists(args[1]):
                 print(f"File not found: {args[1]}")
+                return
+
+            if not os.path.exists(args[2]):
+                print(f"No such directory: {args[2]}")
+                return
+
+            shutil.copy(args[1], args[2])
+            print(f"Copied {args[1]} -> {args[2]}")
             return
 
     #Catch exception if user input is not a valid command
